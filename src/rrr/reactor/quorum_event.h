eda0278429afb0354ea9e5e4648cef72e81298b2
Andrew Yoo <Andrew Yoo>
2019-10-06 10:44:20 -0500

    added logging

--- src/rrr/reactor/quorum_event.h
+++ src/rrr/reactor/quorum_event.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <fstream>
+#include <iostream>
 #include "event.h"
 
 using rrr::Event;
@@ -22,6 +24,7 @@ class QuorumEvent : public Event {
   vector<uint64_t> vec_timestamp_{};
   vector<int> sites_{}; // not sure if SiteInfo or int
   std::unordered_map<int, std::unordered_set<int>> deps{}; //not sure if SiteInfo or int
+  std::string log_file = "logs.txt";
 
   QuorumEvent() = delete;
 
@@ -52,16 +55,44 @@ class QuorumEvent : public Event {
     Test();
   }
 
-  //update_deps separated into two parts for finer control
-  void add_dep(int srcId){
-    std::unordered_set<int> tgtIds = {};
-    for(auto site: sites_){
-      auto index = deps.find(site);
-      if(index == deps.end() && site != srcId){
-        tgtIds.insert(site);
+  void add_dep(int srcId, int tgtId){
+    auto srcIndex = deps.find(srcId);
+    if(srcIndex == deps.end()){
+      std::unordered_set<int> temp = {};
+      deps[srcId] = temp;
+    }
+    auto tgtIndex = deps[srcId].find(tgtId);
+    if(tgtIndex == deps[srcId].end() && srcId != tgtId){
+      deps[srcId].insert(tgtId);
+    }
+  }
+
+  void remove_dep(int srcId, int tgtId){
+    auto srcIndex = deps.find(srcId);
+    if(srcIndex != deps.end()){
+      auto tgtIndex = deps[srcId].find(tgtId);
+      if(tgtIndex != deps[srcId].end()){
+        deps[srcId].erase(tgtId);
       }
     }
-    deps[srcId] = tgtIds;
+  }
+
+  void log(){
+    std::ofstream of(log_file, std::fstream::app);
+    of << "hello\n";
+    for(auto it = deps.begin(); it != deps.end(); it++){
+      of << "{ " << it->first << ": ";
+      for(auto it2 = it->second.begin(); it2 != it->second.end(); it2++){
+        of << *it2 << " ";
+      }
+      of << "}\n";
+    }
+    of.close();
+  }
+
+  void Wait() override{
+    log();
+    Event::Wait();
   }
 
   bool IsReady() override {
