f25db6186cb2f91a2100124fc530d7131882b97b
Andrew Yoo <Andrew Yoo>
2019-11-04 08:31:17 -0600

    WIP

--- src/deptran/classic/coordinator.cc
+++ src/deptran/classic/coordinator.cc
@@ -299,13 +299,36 @@ void CoordinatorClassic::Prepare() {
               partition_id);
     rpc_event->add_dep(commo()->LeaderProxyForPartition(partition_id).first);
     rpc_event->log();
-    commo()->SendPrepare(partition_id,
+    auto phase = phase_;
+    //moving this to communicator might be easier and add a hack for GotoNextPhase
+    //also add this to the call to SendPrepare
+    auto callback = [this, phase](int32_t res){
+      if(this->phase_ != phase) return;
+      TxData* cmd = (TxData*) this->cmd_;
+      if(res == REJECT){
+        cmd->commit_.store(false);
+        this->aborted_ = true;
+      }
+      if(this->n_prepare_ack_ = cmd->partition_ids_.size()){
+        if(!this->aborted_){
+          cmd->commit_.store(true);
+          this->committed_=true;
+        }
+        this->GotoNextPhase();
+      }
+    };
+    /*commo()->SendPrepare(partition_id,
                          cmd_->id_,
                          sids,
                          std::bind(&CoordinatorClassic::PrepareAck,
                                    this,
                                    phase_,
-                                   std::placeholders::_1));
+                                   std::placeholders::_1));*/
+    auto sp_rpc_event = commo()->SendPrepare(partition_id,
+                                            cmd_->id_,
+                                            sids,
+                                            callback);
+    //sp_rpc_event->Wait();
     verify(site_prepare_[partition_id] == 0);
     site_prepare_[partition_id]++;
     verify(site_prepare_[partition_id] == 1);
