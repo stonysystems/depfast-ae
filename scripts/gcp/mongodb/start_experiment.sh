d213cebe9584c5175a52f38969f8836f84ed8ede
ritesh <ritesh_sinha@ymail.com>
2020-06-08 01:56:20 -0700

    Refactor testing script to allow reusability

--- scripts/gcp/mongodb/start_experiment.sh
+++ scripts/gcp/mongodb/start_experiment.sh
@@ -2,20 +2,8 @@
 
 set -ex
 
-if [ "$#" -ne 4 ]; then
-    echo "Illegal number of parameters"
-    echo "1st arg - number of iterations"
-    echo "2nd arg - workload path"
-    echo "3rd arg - seconds to run ycsb run"
-    echo "4th arg - experiment to run(1,2,3,4,5)"
-    exit 1
-fi
-
-iterations=$1
-workload=$2
-ycsbruntime=$3
-expno=$4
-
+# Server specific configs
+##########################
 s1="10.128.0.28"
 s2="10.128.0.14"
 s3="10.128.0.15"
@@ -24,40 +12,73 @@ s1name="andrew-server1"
 s2name="andrew-server2"
 s3name="andrew-server3"
 serverZone="us-central1-a"
+###########################
 
-echo "Running follower experiment $expno for mongodb"
-# Start the servers if they are already not started
+if [ "$#" -ne 6 ]; then
+    echo "Wrong number of parameters"
+    echo "1st arg - number of iterations"
+    echo "2nd arg - workload path"
+    echo "3rd arg - seconds to run ycsb run"
+    echo "4th arg - experiment to run(1,2,3,4,5)"
+    echo "5th arg - host type(gcp/aws)"
+    echo "6th arg - type of experiment(follower/leader/noslow)"
+    exit 1
+fi
 
-mkdir -p results
+iterations=$1
+workload=$2
+ycsbruntime=$3
+expno=$4
+host=$5
+exptype=$6
 
-for (( i=1; i<=$iterations; i++ ))
-do
-	echo "Running experiment $expno - Trial $i"
+# test_start is executed at the beginning
+function test_start {
+	name=$1
+	
+	echo "Running $exptype experiment $expno for $name"
+	dirname="$name"_"$exptype"_results
+	mkdir -p $dirname
+}
 
-	# Start the servers
-	gcloud compute instances start "$s1name" "$s2name" "$s3name" --zone="$serverZone"
-
-	sleep 60
-
-	# 0. Cleanup first
+# data_cleanup is called just after servers start
+function data_cleanup {
 	ssh -i ~/.ssh/id_rsa "$s1" "sh -c 'rm -rf /srv/mongodb/rs0-*/data ; rm -rf /srv/mongodb/rs0-*'"
 	ssh -i ~/.ssh/id_rsa "$s2" "sh -c 'rm -rf /srv/mongodb/rs0-*/data ; rm -rf /srv/mongodb/rs0-*'"
 	ssh -i ~/.ssh/id_rsa "$s3" "sh -c 'rm -rf /srv/mongodb/rs0-*/data ; rm -rf /srv/mongodb/rs0-*'"
+}
 
-	
-	# 2. Create data directories
+# start_servers is used to boot the servers up
+function start_servers {	
+	if [ "$host" == "gcp" ]; then
+		gcloud compute instances start "$s1name" "$s2name" "$s3name" --zone="$serverZone"
+	elif [ "$host" == "aws" ]; then
+		echo "Not implemented error"
+		exit 1
+	else
+		echo "Not implemented error"
+		exit 1
+	fi
+	sleep 60
+}
+
+# init is called to initialise the db servers
+function init {
 	ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'sudo mkdir -p /data ; sudo mkfs.xfs /dev/sdb -f ; sudo mount -t xfs /dev/sdb /data ; sudo mount -t xfs /dev/sdb /data -o remount,noatime ; sudo mkdir /data/db ; sudo chmod o+w /data/db'"
 	ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'sudo mkdir -p /data ; sudo mkfs.xfs /dev/sdb -f ; sudo mount -t xfs /dev/sdb /data ; sudo mount -t xfs /dev/sdb /data -o remount,noatime ; sudo mkdir /data/db ; sudo chmod o+w /data/db'"
 	ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'sudo mkdir -p /data ; sudo mkfs.xfs /dev/sdb -f ; sudo mount -t xfs /dev/sdb /data ; sudo mount -t xfs /dev/sdb /data -o remount,noatime ; sudo mkdir /data/db ; sudo chmod o+w /data/db'"
+}
 
-	# 1. SSH to all the machines and start db
+# start_db starts the database instances on each of the server
+function start_db {
 	ssh  -i ~/.ssh/id_rsa "$s1" "sh -c 'numactl --interleave=all taskset -ac 0 mongod --replSet rs0 --bind_ip localhost,"$s1name" --fork --logpath /tmp/mongod.log'"
 	ssh  -i ~/.ssh/id_rsa "$s2" "sh -c 'numactl --interleave=all taskset -ac 0 mongod --replSet rs0 --bind_ip localhost,"$s2name" --fork --logpath /tmp/mongod.log'"
 	ssh  -i ~/.ssh/id_rsa "$s3" "sh -c 'numactl --interleave=all taskset -ac 0 mongod --replSet rs0 --bind_ip localhost,"$s3name" --fork --logpath /tmp/mongod.log'" 
-
 	sleep 30
+}
 
-	# 2. Init
+# db_init initialises the database
+function db_init {
 	mongo --host "$s1name" < init_script.js
 
 	# Wait for startup
@@ -75,41 +96,105 @@ do
 	secondarypid=$(ssh -i ~/.ssh/id_rsa "$secondaryip" "sh -c 'pgrep mongo'")
 	echo $secondarypid
 
+	if [ "$exptype" == "follower" ]; then
+		slowdownpid=$secondarypid
+		slowdownip=$secondaryip	
+	elif [ "$exptype" == "leader" ]; then
+		slowdownpid=$primarypid
+		slowdownip=$primaryip
+	else
+		# Nothing to do
+		echo ""
+	fi
+
 	# Disable chaining allowed
 	mongo --host $primaryip --eval "cfg = rs.config(); cfg.settings.chainingAllowed = false; rs.reconfig(cfg);"
+}
 
-	# 3. Run ycsb load
+# ycsb_load is used to run the ycsb load and wait until it completes.
+function ycsb_load {
 	./bin/ycsb load mongodb -s -P $workload -p mongodb.url=mongodb://$primaryip:27017/ycsb?w=majority&readConcernLevel=majority ; wait $!
+}
 
-	# 4. Run experiment
-	# Slow down follower
-	./experiment$expno.sh "$secondaryip" "$secondarypid"
+# ycsb run exectues the given workload and waits for it to complete
+function ycsb_run {
+	./bin/ycsb run mongodb -s -P $workload  -p maxexecutiontime=$ycsbruntime -p mongodb.url="mongodb://$primaryip:27017/ycsb?w=majority&readConcernLevel=majority" > "$dirname"/exp"$expno"_trial_"$i".txt ; wait $!
+}
 
-	# 5. ycsb run
-	./bin/ycsb run mongodb -s -P $workload  -p maxexecutiontime=$ycsbruntime -p mongodb.url="mongodb://$primaryip:27017/ycsb?w=majority&readConcernLevel=majority" > results/exp"$expno"_trial_"$i".txt ; wait $!
-
-	# 6. cleanup
+# cleanup is called at the end of the given trial of an experiment
+function cleanup {
 	mongo --host "$primaryip" < cleanup_script.js
 	mongo --host "$primaryip" --eval "db.getCollectionNames().forEach(function(n){db[n].remove()});"
-	# Cleanup secondary
-	#ssh -i ~/.ssh/id_rsa "$secondaryip" "sudo sh -c 'sudo pkill -f 'dd if=/dev/zero' ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db'" ; true
 	ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'rm -rf /data/db ; sudo umount /dev/sdb ; sudo rm -rf /data/ ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db ; pkill mongod ; true'"
 	ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'rm -rf /data/db ; sudo umount /dev/sdb ; sudo rm -rf /data/ ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db ; pkill mongod ; true'"
 	ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'rm -rf /data/db ; sudo umount /dev/sdb ; sudo rm -rf /data/ ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db ; pkill mongod ; true'"
 	# Remove the tc rule for exp 5
-	if [ "$expno" == 5 ]; then
-		ssh -i ~/.ssh/id_rsa "$secondaryip" "sudo sh -c 'sudo /sbin/tc qdisc del dev ens4 root'"
+	if [ "$expno" == 5 -a "$exptype" != "noslow" ]; then
+		ssh -i ~/.ssh/id_rsa "$slowdownip" "sudo sh -c 'sudo /sbin/tc qdisc del dev ens4 root ; true'"
 	fi
 	rm result.json
-
 	sleep 5
-	
-	# 8. Power off all the VMs
-	gcloud compute instances stop "$s1name" "$s2name" "$s3name" --zone="$serverZone"
+}
 
-done
+# stop_servers turns off the VM instances
+function stop_servers {
+	if [ "$host" == "gcp" ]; then
+		gcloud compute instances stop "$s1name" "$s2name" "$s3name" --zone="$serverZone"
+	elif [ "$host" == "aws" ]; then
+		echo "Not implemented error"
+		exit 1
+	else
+		echo "Not implemented error"
+		exit 1
+	fi
+}
 
-# Shut down the servers
-gcloud compute instances stop "$s1name" "$s2name" "$s3name" --zone="$serverZone"
-#Make sure either shutdown is executed when you run this script or uncomment the last line
+# run_experiment executes the given experiment
+function run_experiment {
+	./experiment$expno.sh "$slowdownip" "$slowdownpid"
+}
+
+# test_run is the main driver function
+function test_run {
+	for (( i=1; i<=$iterations; i++ ))
+	do
+		echo "Running experiment $expno - Trial $i"
+		# 1. start servers
+		start_servers
+
+		# 2. Cleanup first
+		data_cleanup	
+
+		# 3. Create data directories
+		init
+
+		# 4. SSH to all the machines and start db
+		start_db
+
+		# 5. Init
+		db_init
+
+		# 6. ycsb load
+		ycsb_load
+
+		# 7. Run experiment if this is not a no slow
+		if [ "$exptype" != "noslow" ]; then
+			run_experiment
+		fi
+
+		# 8. ycsb run
+		ycsb_run
+
+		# 9. cleanup
+		cleanup
+		
+		# 10. Power off all the VMs
+		stop_servers
+	done
+}
+
+test_start mongodb
+test_run
+
+# Make sure either shutdown is executed after you run this script or uncomment the last line
 # sudo shutdown -h now
