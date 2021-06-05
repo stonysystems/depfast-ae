e363a4155740dc506786e781a653ee7c89e1156f
Jiannan <1457888782@qq.com>
2021-06-05 23:14:17 +0800

    Modify rocksdb code

--- src/rdb/rocksdb_wrapper.cc
+++ src/rdb/rocksdb_wrapper.cc
@@ -1,7 +1,14 @@
 #include "rocksdb_wrapper.h"
 
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <mutex>
-using namespace std;
 
 namespace rdb {
 
@@ -22,19 +29,216 @@ void rocksdb_wrapper_delete() {
   }
 }
 
-int RocksdbWrapper::do_add_column(const char* name) {
-  // create column family
-  ColumnFamilyOptions c_option{};
-  ColumnFamilyHandle* cf;
-  s = db->CreateColumnFamily(c_option, name, &cf);
-  if (!s.ok()) {
-    return -1;
+static bool Mkdirs(const char* dir) {
+  char path_buf[1024];
+  const int dir_mode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
+  char* p = NULL;
+  size_t len = 0;
+  snprintf(path_buf, sizeof(path_buf), "%s", dir);
+  len = strlen(path_buf);
+  if (path_buf[len - 1] == '/') {
+    path_buf[len - 1] = 0;
+  }
+  for (p = path_buf + 1; *p; p++) {
+    if (*p == '/') {
+      *p = 0;
+      int status = mkdir(path_buf, dir_mode);
+      if (status != 0 && errno != EEXIST) {
+        return false;
+      }
+      *p = '/';
+    }
+  }
+  int status = mkdir(path_buf, dir_mode);
+  if (status != 0 && errno != EEXIST) {
+    return false;
+  }
+  return true;
+}
+
+const std::string RocksdbWrapper::anonymous_user = "";
+
+RocksdbWrapper::RocksdbWrapper(const std::string& data_dir)
+    : data_dir_(data_dir) {
+  bool ok = Mkdirs(data_dir.c_str());
+  assert(ok == true);
+  // Create default database for shared namespace, i.e. anonymous user
+  std::string full_name = data_dir + "/@db";
+
+  Options options;
+  // Optimize RocksDB.
+  options.IncreaseParallelism();
+  options.OptimizeLevelStyleCompaction();
+  // create the DB if it's not already present
+  // This is a new database.
+  options.create_if_missing = true;
+  DB* default_db = NULL;
+  Status status = DB::Open(options, full_name, &default_db);
+  assert(status.ok());
+  dbs_[""] = default_db;
+}
+
+RocksdbWrapper::~RocksdbWrapper() {
+  MutexLock lock(&mu_);
+  for (std::map<std::string, DB*>::iterator it = dbs_.begin(); it != dbs_.end();
+       ++it) {
+    delete it->second;
+    it->second = NULL;
+  }
+}
+
+bool RocksdbWrapper::OpenDatabase(const std::string& name) {
+  {
+    MutexLock lock(&mu_);
+    if (dbs_.find(name) != dbs_.end()) {
+      return true;
+    }
   }
 
-  handles.push_back(cf);
-  insert_into_map(col_name_to_id_, cf->GetName(), cf->GetID());
+  std::string full_name = data_dir_ + "/" + name + "@db";
+  Options options;
+  // Optimize RocksDB.
+  options.IncreaseParallelism();
+  options.OptimizeLevelStyleCompaction();
+  // create the DB if it's not already present
+  // This is a new database.
+  options.create_if_missing = true;
+  DB* current_db = NULL;
+  Status status = DB::Open(options, full_name, &current_db);
 
-  return cf->GetID();
+  assert(status.ok());
+  {
+    MutexLock lock(&mu_);
+    dbs_[name] = current_db;
+  }
+  return status.ok();
+}
+
+void RocksdbWrapper::CloseDatabase(const std::string& name) {
+  MutexLock lock(&mu_);
+  std::map<std::string, DB*>::iterator dbs_it = dbs_.find(name);
+  if (dbs_it == dbs_.end()) {
+    return;
+  }
+  delete dbs_it->second;
+  dbs_it->second = NULL;
+  dbs_.erase(dbs_it);
+}
+
+bool RocksdbWrapper::Get(const std::string& name, const std::string& key,
+                         std::string* value) {
+  if (value == NULL) {
+    return false;
+  }
+  DB* db_ptr = NULL;
+  {
+    MutexLock lock(&mu_);
+    if (dbs_.find(name) == dbs_.end()) {
+      return false;
+    }
+    db_ptr = dbs_[name];
+    if (db_ptr == NULL) {
+      return false;
+    }
+  }
+  Status status = db_ptr->Get(ReadOptions(), key, value);
+  return (status.ok());
+}
+
+bool RocksdbWrapper::Put(const std::string& name, const std::string& key,
+                         const std::string& value) {
+  DB* db_ptr = NULL;
+  {
+    MutexLock lock(&mu_);
+    if (dbs_.find(name) == dbs_.end()) {
+      return false;
+    }
+    db_ptr = dbs_[name];
+    if (db_ptr == NULL) {
+      return false;
+    }
+  }
+  Status status = db_ptr->Put(WriteOptions(), key, value);
+  return (status.ok());
+}
+
+bool RocksdbWrapper::Put(const std::string& name, const std::string& key,
+                         const Slice& value) {
+  DB* db_ptr = NULL;
+  {
+    MutexLock lock(&mu_);
+    if (dbs_.find(name) == dbs_.end()) {
+      return false;
+    }
+    db_ptr = dbs_[name];
+    if (db_ptr == NULL) {
+      return false;
+    }
+  }
+  Status status = db_ptr->Put(WriteOptions(), key, value);
+  return (status.ok());
+}
+
+bool RocksdbWrapper::Delete(const std::string& name, const std::string& key) {
+  DB* db_ptr = NULL;
+  {
+    MutexLock lock(&mu_);
+    if (dbs_.find(name) == dbs_.end()) {
+      return false;
+    }
+    db_ptr = dbs_[name];
+    if (db_ptr == NULL) {
+      return false;
+    }
+  }
+  Status status = db_ptr->Delete(WriteOptions(), key);
+
+  return (status.ok());
+}
+
+std::string RocksdbWrapper::Iterator::key() const {
+  return (it_ != NULL) ? it_->key().ToString() : "";
+}
+
+std::string RocksdbWrapper::Iterator::value() const {
+  return (it_ != NULL) ? it_->value().ToString() : "";
+}
+
+RocksdbWrapper::Iterator* RocksdbWrapper::Iterator::Seek(std::string key) {
+  if (it_ != NULL) {
+    it_->Seek(key);
+  }
+  return this;
+}
+
+RocksdbWrapper::Iterator* RocksdbWrapper::Iterator::Next() {
+  if (it_ != NULL) {
+    it_->Next();
+  }
+  return this;
+}
+
+bool RocksdbWrapper::Iterator::Valid() const {
+  return (it_ != NULL) ? it_->Valid() : false;
+}
+
+Status RocksdbWrapper::Iterator::status() const { return it_->status(); }
+
+RocksdbWrapper::Iterator* RocksdbWrapper::NewIterator(const std::string& name) {
+  DB* db_ptr = NULL;
+  {
+    MutexLock lock(&mu_);
+    std::map<std::string, DB*>::iterator dbs_it = dbs_.find(name);
+    if (dbs_it == dbs_.end()) {
+      return NULL;
+    }
+    db_ptr = dbs_it->second;
+    if (db_ptr == NULL) {
+      return NULL;
+    }
+  }
+
+  return new RocksdbWrapper::Iterator(db_ptr, ReadOptions());
 }
 
 }  // namespace rdb
--- src/rdb/rocksdb_wrapper.h
+++ src/rdb/rocksdb_wrapper.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <vector>
 
+#include "base/threading.hpp"
 #include "rocksdb/db.h"
 #include "rocksdb/options.h"
 #include "rocksdb/slice.h"
@@ -15,143 +16,85 @@ namespace rdb {
 
 using namespace ROCKSDB_NAMESPACE;
 
-using colid_t = int;
+using namespace rrr;
 
 const std::string default_db_path = "/db";
 
 class RocksdbWrapper {
  public:
-  // It is the type for store byte stream.
+  static const std::string anonymous_user;
   static Slice MakeSlice(char* data, size_t n) { return Slice(data, n); }
+  RocksdbWrapper(const std::string& data_dir);
+  ~RocksdbWrapper();
 
-  RocksdbWrapper(std::string path = default_db_path) : db_path(path) {
-    Options options;
-    // Optimize RocksDB.
-    options.IncreaseParallelism();
-    options.OptimizeLevelStyleCompaction();
-    // create the DB if it's not already present
+  bool OpenDatabase(const std::string& name);
+  void CloseDatabase(const std::string& name);
 
-    // List all column family
-    std::vector<std::string> column_families_list;
-    s = DB::ListColumnFamilies(options, db_path, &column_families_list);
+  // The name is the different database.
+  bool Get(const std::string& name, const std::string& key, std::string* value);
+  bool Put(const std::string& name, const std::string& key,
+           const std::string& value);
+  bool Put(const std::string& name, const std::string& key, const Slice& value);
+  bool Delete(const std::string& name, const std::string& key);
 
-    if (!s.ok()) {
-      // This is a new database.
-      options.create_if_missing = true;
-      s = DB::Open(options, db_path, &db);
-      // optional
-      assert(s.ok());
-      handles.push_back(db->DefaultColumnFamily());
-      insert_into_map(col_name_to_id_, db->DefaultColumnFamily()->GetName(),
-                      db->DefaultColumnFamily()->GetID());
-    } else {
-      std::vector<ColumnFamilyDescriptor> column_families;
-      for (auto column_name : column_families_list) {
-        column_families.push_back(
-            ColumnFamilyDescriptor(column_name, ColumnFamilyOptions()));
+  bool Get(const std::string& key, std::string* value) {
+    return Get(anonymous_user, key, value);
+  }
+  bool Put(const std::string& key, const std::string& value) {
+    return Put(anonymous_user, key, value);
+  }
+  bool Put(const std::string& key, const Slice& value) {
+    return Put(anonymous_user, key, value);
+  }
+
+  bool Delete(const std::string& key) { return Delete(anonymous_user, key); }
+
+ public:
+  class Iterator {
+   public:
+    Iterator() : it_(NULL) {}
+    Iterator(DB* db, const ReadOptions& option) {
+      it_ = db->NewIterator(option);
+    }
+    ~Iterator() {
+      if (it_ != NULL) {
+        delete it_;
+        it_ = NULL;
       }
-      s = DB::Open(options, db_path, column_families, &handles, &db);
-      assert(s.ok());
-
-      for (auto handle : handles) {
-        insert_into_map(col_name_to_id_, handle->GetName(), handle->GetID());
-      }
     }
-  }
 
-  ~RocksdbWrapper() {
-    // close db
-    for (auto handle : handles) {
-      // s = db->DestroyColumnFamilyHandle(handle);
-      assert(s.ok());
-    }
-    delete db;
-  }
+    std::string key() const;
+    std::string value() const;
 
-  // Add ColumnFamily.
-  int add_column(const char* name) { return do_add_column(name); }
+    Iterator* Seek(std::string key);
+    Iterator* Next();
 
-  colid_t get_column_id(std::string& name) {
-    auto it = col_name_to_id_.find(name);
-    if (it != std::end(col_name_to_id_)) {
-      return col_name_to_id_[name];
-    }
-    return -1;
-  }
+    bool Valid() const;
+    Status status() const;
 
-  ColumnFamilyHandle* get_column_handle(std::string& name) {
-    colid_t col_id = get_column_id(name);
-    if (col_id < 0) {
-      return nullptr;
-    } else {
-      return handles[col_id];
-    }
-  }
+   private:
+    rocksdb::Iterator* it_;
+  };
 
-  ColumnFamilyHandle* get_column_handle(colid_t column_id) {
-    return handles[column_id];
-  }
-
-  rocksdb::Status GetDbStatus() { return s; }
-
-  bool SetOption(std::string& key, std::string& value) {
-    s = db->SetOptions({{key, value}});
-    return s.ok();
-  }
-
-  // GET
-  Slice Get(const Slice& key) { return Get(db->DefaultColumnFamily(), key); }
-
-  Slice Get(ColumnFamilyHandle* column_handle, const Slice& key) {
-    PinnableSlice val;
-    s = db->Get(ReadOptions(), column_handle, key, &val);
-    if (!s.ok()) {
-      // give some error info.
-    }
-    return val;
-  }
-
-  // PUT
-  bool Put(const Slice& key, Slice& val) {
-    return Put(db->DefaultColumnFamily(), key, val);
-  }
-
-  bool Put(ColumnFamilyHandle* column_handle, const Slice& key, Slice& val) {
-    WriteOptions w;
-    // w.sync = true;
-    s = db->Put(w, column_handle, key, val);
-    return s.ok();
-  }
-
-  // DELETE
-  bool Delete(const Slice& key) {
-    return Delete(db->DefaultColumnFamily(), key);
-  }
-
-  bool Delete(ColumnFamilyHandle* column_handle, const Slice& key) {
-    s = db->Delete(WriteOptions(), key);
-    return s.ok();
-  }
-
-  typedef std::vector<ColumnFamilyHandle*>::const_iterator iterator;
-  iterator begin() const { return std::begin(handles); }
-  iterator end() const { return std::end(handles); }
-  size_t columns_count() const { return handles.size(); }
-
- protected:
-  std::string db_path;
-  DB* db;
-  Status s;
-
-  std::vector<ColumnFamilyHandle*> handles;
-  std::unordered_map<std::string, colid_t> col_name_to_id_;
+  Iterator* NewIterator(const std::string& name);
 
  private:
-  int do_add_column(const char* name);
-  void insert_into_map(std::unordered_map<std::string, colid_t> map,
-                       std::string key, colid_t val) {
-    map.insert(std::make_pair(key, val));
-  }
+  Mutex mu_;
+  std::string data_dir_;
+  std::map<std::string, DB*> dbs_;
+};
+
+// Help class for mutex
+class MutexLock {
+ public:
+  explicit MutexLock(Mutex* mu) : mu_(mu) { this->mu_->lock(); }
+  // No copying allowed
+  MutexLock(const MutexLock&) = delete;
+  void operator=(const MutexLock&) = delete;
+  ~MutexLock() { this->mu_->unlock(); }
+
+ private:
+  Mutex* const mu_;
 };
 
 void rocksdb_wrapper_delete();
