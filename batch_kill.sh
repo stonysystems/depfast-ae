e3d709f2cbab398f8e9db83de0e90467781d2547
Xuhao Luo <rossihox@gmail.com>
2022-05-17 23:16:56 +0000

    add batch kill; update slowdown exp

---
+++ batch_kill.sh
@@ -0,0 +1,10 @@
+ps aux  |  grep -i depfast |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run.py |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run_exp1.py |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run_exp2.py  |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run_exp3.py  |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run_exp4.py  |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run_exp5.py  |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i run_exp6.py  |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux  |  grep -i inf |  awk '{print $2}'  |  xargs sudo kill -9
+sleep 1
