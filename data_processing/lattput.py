29feb13274f3981b43999870f0a95222c0a19db0
shenweihai1 <shenweihai1@gmail.com>
2022-05-18 15:56:46 -0400

    add automatic plot for figure5

---
+++ data_processing/lattput.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import processing
+
+def getdata(proto, r):
+    # figure5a, raft: 3,5
+    # figure6a, copilot: 3
+    lat = []
+    tput = []
+    if proto == "raft":
+        data_3, data_5 = processing.figure5a()
+        if r == 3:
+            for e in data_3:
+                lat.append(e[1])
+                tput.append(e[2])
+        else:
+            for e in data_5:
+                lat.append(e[1])
+                tput.append(e[2])
+    else:  # copilot
+        data_r = processing.figure5b()
+        for e in data_r:
+            lat.append(e[1])
+            tput.append(e[2])
+    
+    return lat, tput
+
+def plot_lattput(proto, rep, ax, plt_id):
+    proto_set = {
+        'raft': ['raft'],
+        'copilot': ['copilot']
+    }
+
+    real_name = {
+	'copilot': 'DepFast-Copilot',
+	'copilot.ref': 'Copilot',
+	'raft': 'DepFast-Raft',
+	'etcd': 'etcd',
+	'etcd.one': 'etcd (1-core)'
+    }
+
+    marker = {
+        'copilot': '-x',
+        'copilot.ref': '-^',
+        'raft': '-x',
+        'etcd': '-^',
+        'etcd.one': '-v'
+    }
+
+    annotate = {
+	'copilot': 11,
+	'raft': 9
+    }
+
+    for p in proto_set[proto]:
+        for r in rep:
+            lat, tput = getdata(p, r)
+
+            ax.plot(tput, lat, marker[p], label='{} {}-rep'.format(real_name[p], r),
+					linewidth=3, ms=6 if p == 'etcd' else 8, markeredgewidth=3)
+            try:
+                x = tput[annotate[p]]
+                y = lat[annotate[p]]
+                ax.plot(x, y, 'r*', label='_nolegend_', ms=12, markeredgewidth=3)
+            except:
+                pass
+
+    ax.set_xlabel('Throughput (op/s)')
+    ax.set_ylabel('Med latency (ms)')
+    ax.set_xlim(0)
+    ax.set_ylim(0)
+
+    ax.legend(frameon=False, fontsize='small')
+    ax.set_box_aspect(0.6)
+    ax.set_title('{} Latency-Throughput'.format(plt_id), y=-0.45, fontsize=18, fontweight='bold')
+
+
+if __name__ == "__main__":
+    fig, ax = plt.subplots()
+    plot_lattput('raft', [3], ax, '')
--- data_processing/processing.py
+++ data_processing/processing.py
@@ -2,6 +2,9 @@ import yaml
 import json
 import os
 
+# the library for the data-processing
+BASE="../"
+
 def convert_yml_json(name):
   with open(name, 'r') as file:
       configuration = yaml.safe_load(file)
@@ -12,32 +15,35 @@ def convert_yml_json(name):
   return json.load(open(name+".json"))
 
 def find_the_yml(folder_name):
+    if not os.path.isdir(folder_name):
+        return ""
+
     for filename in os.listdir(folder_name):
         f = os.path.join(folder_name, filename)
         if os.path.isfile(f):
-            if ".yml" in filename:
+            if ".yml" in filename and ".json" not in filename:
                 return folder_name + "/" + filename
     return ""
 
 def figure5a():
     # 3 replicas
     conc=[20, 40, 60, 80, 100, 130, 160, 190, 220, 260, 300, 340, 380, 420]
-    data_3=[] # (currency, 50th, tps)
+    data_3=[] # (currency, 50th, tps, 99th)
     for i in conc:
-        folder="./figure5a/results_3_"+str(i)
+        folder=BASE+"figure5a/results_3_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_3.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_3.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]["99"]))
         else:
             print("No yml found in " + folder)
     
     # 5 replicas
     data_5=[]
     for i in conc:
-        folder="./figure5a/results_5_"+str(i)
+        folder=BASE+"figure5a/results_5_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_5.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_5.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]["99"]))
         else:
             print("No yml found in " + folder)
     return data_3, data_5
@@ -46,35 +52,36 @@ def figure5a():
 def figure5b():
     # 3 replicas
     exp=[1, 2, 3, 4, 5, 6]
-    data_3=[] # (exp, 50th, tps)
+    data_3=[] # (exp, 50th, tps, all_latency)
     for i in exp:
-        folder="./figure5b/results_3_"+str(i)
+        folder=BASE+"/figure5b/results_3_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_3.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_3.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]))
         else:
             print("No yml found in " + folder)
     
     # 5 replicas
     data_5=[]
     for i in exp:
-        folder="./figure5b/results_5_"+str(i)
+        folder=BASE+"/figure5b/results_5_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_5.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_5.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]))
         else:
             print("No yml found in " + folder)
 
     return data_3, data_5
 
+
 def figure6a():
     conc=[1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-    data_r=[]
+    data_r=[] # (currency, 50th, tps, 99th)
     for i in conc:
-        folder="./figure6a/results_"+str(i)
+        folder=BASE+"/figure6a/results_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_r.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_r.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]["99"]))
         else:
             print("No yml found in " + folder)
     
@@ -83,22 +90,22 @@ def figure6a():
 def figure6b():
     # leader
     exp=[1, 2, 5, 6]
-    data_l=[] # (exp, 50th, tps)
+    data_l=[] # (exp, 50th, tps, all_latency)
     for i in exp:
-        folder="./figure6b/results_leader_"+str(i)
+        folder=BASE+"/figure6b/results_leader_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_l.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_l.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]))
         else:
             print("No yml found in " + folder)
     
     # follower
     data_f=[]
     for i in exp:
-        folder="./figure6b/results_follower_"+str(i)
+        folder=BASE+"/figure6b/results_follower_"+str(i)
         if find_the_yml(folder):
             data=convert_yml_json(find_the_yml(folder))
-            data_f.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"]))
+            data_f.append((i, data["PAYMENT"]["all_latency"]["50"], data["PAYMENT"]["tps"], data["PAYMENT"]["all_latency"]))
         else:
             print("No yml found in " + folder)
 
@@ -106,7 +113,8 @@ def figure6b():
 
 
 if __name__ == "__main__":
-    print(figure5a())
-    print(figure5b())
-    print(figure6a())
-    print(figure6b())
+    #print(figure5a())
+    #print(figure5b())
+    # print(figure6a())
+    # print(figure6b())
+    pass
