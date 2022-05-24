697b2f2ba359b8d14d0fa97bfe0c75356e6d330c
Debian <azureuser@evaluator-client-1.zuqd2vcnutourlcgnq4qu3z2hb.bx.internal.cloudapp.net>
2022-05-24 01:50:22 +0000

    add more dependencies

--- dep.sh
+++ dep.sh
@@ -17,9 +17,12 @@ sudo apt-get --assume-yes install -y \
     python3-pip \
     python3-wheel \
     python3-setuptools \
+    libjpeg-dev \
+    zlib1g-dev \
     libgoogle-perftools-dev
 
 sudo wget https://github.com/mikefarah/yq/releases/download/v4.24.2/yq_linux_amd64 \
     -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
 
-pip3 install pyyaml
+pip3 install -r requirements.txt
+pip3 install Pillow matplotlib pyyaml
