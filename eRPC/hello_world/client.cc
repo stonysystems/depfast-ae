6394edeb65109ddaf255c2b6799eef8b5c409a7d
shenweihai1 <shenweihai1@gmail.com>
2022-07-29 18:47:07 +0000

    erpc simple application

--- eRPC/hello_world/client.cc
+++ eRPC/hello_world/client.cc
@@ -1,11 +1,71 @@
 #include "common.h"
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <iterator>
+#include <iostream>
+#include<cstdio>
+#include<ctime>
+#include <chrono>
+
 erpc::Rpc<erpc::CTransport> *rpc;
 erpc::MsgBuffer req;
 erpc::MsgBuffer resp;
+int session_num;
 
-void cont_func(void *, void *) { printf("received %s\n", resp.buf_); }
+void cont_func(void *, void *) { } //printf("received %s\n", resp.buf_); }
 
 void sm_handler(int, erpc::SmEventType, erpc::SmErrType, void *) {}
+
+
+void add(std::vector<int>& _req){
+  //serialize vector into string
+        std::ostringstream oss;
+        if (!_req.empty()){
+            // Convert all but the last element to avoid a trailing ","
+            std::copy(_req.begin(), _req.end()-1,
+                std::ostream_iterator<int>(oss, ","));
+
+            // Now add the last element with no delimiter
+            oss << _req.back();
+        }   
+        std::string str = oss.str();
+        const char* chr = str.c_str();
+        req = rpc->alloc_msg_buffer_or_die(strlen(chr));
+        sprintf(reinterpret_cast<char *>(req.buf_), "%s", chr);
+        resp = rpc->alloc_msg_buffer_or_die(kMsgSize);
+
+        rpc->enqueue_request(session_num, 2, &req, &resp, cont_func, nullptr);
+        rpc->run_event_loop(100);
+
+}
+
+
+void hello(std::vector<int>& _req){
+//serialize vector into string
+ 
+        std::ostringstream oss;
+        if (!_req.empty()){
+            // Convert all but the last element to avoid a trailing ","
+            std::copy(_req.begin(), _req.end()-1,
+                std::ostream_iterator<int>(oss, ","));
+
+            // Now add the last element with no delimiter
+            oss << _req.back();
+        }   
+        std::string str =  oss.str();
+        const char* chr = str.c_str();
+        req = rpc->alloc_msg_buffer_or_die(strlen(chr));
+        sprintf(reinterpret_cast<char *>(req.buf_), "%s", chr);
+        resp = rpc->alloc_msg_buffer_or_die(kMsgSize);
+
+        rpc->enqueue_request(session_num, 1, &req, &resp, cont_func, nullptr);
+        rpc->run_event_loop(100);
+
+}
+
 
 int main() {
   std::string client_uri = kClientHostname + ":" + std::to_string(kUDPPortClient);
@@ -14,17 +74,33 @@ int main() {
   rpc = new erpc::Rpc<erpc::CTransport>(&nexus, nullptr, 0, sm_handler);
 
   std::string server_uri = kServerHostname + ":" + std::to_string(kUDPPort);
-  int session_num = rpc->create_session(server_uri, 0);
+  session_num = rpc->create_session(server_uri, 0);
 
   while (!rpc->is_connected(session_num)) rpc->run_event_loop_once();
-  printf("connect to server...");
+  printf("connect to server...\n");
+  
 
-  req = rpc->alloc_msg_buffer_or_die(kMsgSize);
-  sprintf(reinterpret_cast<char *>(req.buf_), "%s", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-  resp = rpc->alloc_msg_buffer_or_die(kMsgSize);
+  //HELLO
+  std::vector<int> hello_req;
+  for(int i = 1; i <= 5; i ++){
+    hello_req.push_back(i);
+  }
+  hello(hello_req);
 
-  rpc->enqueue_request(session_num, kReqType, &req, &resp, cont_func, nullptr);
-  rpc->run_event_loop(100);
 
+  //ADD
+  int n = 100;
+  auto start = std::chrono::high_resolution_clock::now();
+  for(int i = 0; i < n; i ++){  
+    std::vector<int> hello_req;
+    hello_req.push_back(i);
+    hello_req.push_back(i + 1);
+    hello(hello_req);
+  }
+  auto stop = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+  std::cout << "eRPC add time: " << duration.count() << " microsseconds" << std::endl;
   delete rpc;
 }
--- eRPC/hello_world/server.cc
+++ eRPC/hello_world/server.cc
@@ -1,22 +1,73 @@
 #include "common.h"
+
+
 using namespace std;
 erpc::Rpc<erpc::CTransport> *rpc;
 
-void req_handler(erpc::ReqHandle *req_handle, void *) {
-  printf("received %s\n", reinterpret_cast<char *>(req_handle->get_req_msgbuf()->buf_));
+
+void hello_func(vector<int> _req){
+  std::cout << "received a hello request" << std::endl;
+  for(int i : _req)
+    cout << "hello => " << i << endl;
+}
+
+void add_func(int x, int y){
+  std::cout << "received an add request" << std::endl;
+  int z = x + y;
+  cout << "Sum of " << x << " and " << y << " equals " << z << std::endl;
+}
+
+void add_wrapper(erpc::ReqHandle *req_handle, void *) {
+  std::string s = reinterpret_cast<char *>(req_handle->get_req_msgbuf()->buf_);
+  //decode string back into vector
+  std::vector<int> vect;
+  std::stringstream ss(s);
+  for (int i; ss >> i;) {
+      vect.push_back(i);    
+      if (ss.peek() == ',')
+        ss.ignore();
+  }
+  int x = vect[0];
+  int y = vect[1];
+  add_func(x, y);
+
   auto &resp = req_handle->pre_resp_msgbuf_;
-  rpc->resize_msg_buffer(&resp, kMsgSize);
-  sprintf(reinterpret_cast<char *>(resp.buf_), "[message from server]");
+  rpc->resize_msg_buffer(&resp, strlen("add request was processed"));
+  sprintf(reinterpret_cast<char *>(resp.buf_), "add request was processed");
+  rpc->enqueue_response(req_handle, &resp);
+}
+
+void hello_wrapper(erpc::ReqHandle *req_handle, void *) {
+  std::string s = reinterpret_cast<char *>(req_handle->get_req_msgbuf()->buf_);
+  //decode string back into vector
+  std::vector<int> vect;
+  std::stringstream ss(s);
+  for (int i; ss >> i;) {
+      vect.push_back(i);    
+      if (ss.peek() == ',')
+        ss.ignore();
+  }
+  
+  hello_func(vect);
+
+  auto &resp = req_handle->pre_resp_msgbuf_;
+  rpc->resize_msg_buffer(&resp, strlen("hello request was processed"));
+  sprintf(reinterpret_cast<char *>(resp.buf_), "hello request was processed");
   rpc->enqueue_response(req_handle, &resp);
 }
 
 int main() {
   std::string server_uri = kServerHostname + ":" + std::to_string(kUDPPort);
   std::cout << "server_uri: " <<  server_uri << std::endl;
-
   erpc::Nexus nexus(server_uri);
-  nexus.register_req_func(kReqType, req_handler);
+  
+  nexus.register_req_func(1, hello_wrapper);
+  nexus.register_req_func(2, add_wrapper);
 
   rpc = new erpc::Rpc<erpc::CTransport>(&nexus, nullptr, 0, nullptr);
-  rpc->run_event_loop(100000);
+  while (true) {
+    int num_pkts = rpc->run_event_loop_once();
+    if (num_pkts > 0)
+      std::cout << "num_pkts: " << num_pkts << std::endl;
+  }
 }
